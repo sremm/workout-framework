b06946097f411960753794425f15f4e773ec50dc
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-15 17:11:27 +0100

    update Set dataclass

--- wof/domain/types.py
+++ wof/domain/types.py
@@ -7,10 +7,25 @@ from uuid import UUID
 
 
 @dataclass
+class Excercise:
+    """
+    Descriptor for an exercise
+    Has a unique id, but as with good childern, can have many names
+    """
+
+    id: int
+    names: List[str]
+
+
+@dataclass
 class Set:
     """ Class to define a workout set """
 
     excercise: Union[str, List[str]]
+    reps: Union[int, List[int]]
+    weights: Union[float, List[float]]
+    unit: str = "kg"
+    set_number: int = 0  # used in Intensity app as "Set"
 
 
 @dataclass
