44cb447b3d8360ec4387d2e7565fcf4e3f7b1906
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-15 17:20:33 +0100

    add a streamlit data analysis script

---
+++ scripts/st_data_analysis.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import pandas as pd
+
+from wof.repository import intensity_app
+
+import streamlit as st
+
+data_path = list(Path("data", "Intensity").glob("*.csv"))[0]
+
+all_sessions = intensity_app.import_from_file(data_path)
+
+st.write(f"Total sessions: {len(all_sessions)}")
