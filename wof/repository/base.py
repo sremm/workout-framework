16a54c2de11b0d843ca47a0a6ae80253c8519b54
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-19 06:49:51 +0100

    add get method

--- wof/repository/base.py
+++ wof/repository/base.py
@@ -1,8 +1,12 @@
 from abc import ABC
+from uuid import UUID
 from wof.domain.types import Session
 from typing import List
 
 
 class BaseRepository(ABC):
     def add(self, sessions: List[Session]) -> None:
+        raise NotImplementedError
+
+    def get(self, ids: List[UUID]) -> List[Session]:
         raise NotImplementedError
--- wof/repository/csv.py
+++ wof/repository/csv.py
@@ -1,4 +1,5 @@
 from typing import List
+from uuid import UUID
 
 from wof.domain.types import Session
 from wof.repository.base import BaseRepository
@@ -7,3 +8,6 @@ from wof.repository.base import BaseRepository
 class CSVRepository(BaseRepository):
     def add(self, sessions: List[Session]) -> None:
         pass
+
+    def get(self, ids: List[UUID]) -> List[Session]:
+        return []
