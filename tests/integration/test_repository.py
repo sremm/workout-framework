e170069176f1b497d2a96658ef82fc26b20d7e07
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-19 07:15:58 +0100

    update test

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -1,12 +1,12 @@
 from wof.repository.csv import CSVRepository
 from wof.domain.types import Session
-from uuid import UUID
+from uuid import uuid4
 
 
 def test_repository_can_add_and_return_session():
     repository = CSVRepository()
-    session_id = UUID("abc")
-    session_to_add = Session(sets=[], id=session_id)
-    repository.add([session_to_add])
+    session_id = uuid4()
+    sessions_to_add = [Session(sets=[], id=session_id)]
+    repository.add(sessions_to_add)
     session_fetched = repository.get([session_id])
-    assert session_to_add == session_fetched
+    assert sessions_to_add == session_fetched
