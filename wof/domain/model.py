70c2e58c11cbcfacb8c5346d6dbd2988886e449e
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-21 17:51:55 +0100

    update domain model

--- wof/domain/model.py
+++ wof/domain/model.py
@@ -7,7 +7,7 @@ from uuid import UUID
 
 
 @dataclass
-class Excercise:
+class Exercise:
     """
     Descriptor for an exercise
     Has a unique id, but as with good childern, can have many names
@@ -21,11 +21,19 @@ class Excercise:
 class Set:
     """ Class to define a workout set """
 
-    excercise: Union[str, List[str]]
-    reps: Union[int, List[int]]
-    weights: Union[float, List[float]]
+    exercise: Union[str, List[str]] = "name"
+    reps: Union[int, List[int]] = 0
+    weights: Union[float, List[float]] = 0
     unit: str = "kg"
     set_number: int = 0  # used in Intensity app as "Set"
+
+    def __len__(self) -> int:
+        if type(self.exercise) == list:
+            return len(self.exercise)
+        elif type(self.exercise) == str:
+            return 1
+        else:
+            raise TypeError(f"Unexpected type for exercise: {type(self.exercise)}")
 
 
 @dataclass
@@ -35,3 +43,9 @@ class Session:
     sets: List[Set] = field(default_factory=list)
     id: UUID = field(default_factory=uuid4)
     date_time: datetime = field(default_factory=datetime.now)
+
+    def add_sets(self, sets: List[Set]) -> None:
+        pass
+
+    def __len__(self) -> int:
+        return len(self.sets)
