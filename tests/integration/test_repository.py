ea86cd72c702b4e5986b3e51e840bdace55a4fce
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-21 17:17:56 +0100

    add test for saving and loading

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -24,15 +24,28 @@ class TestCSVRepository:
     def test_add_and_get(self):
         repository = CSVRepository()
         session_id = uuid4()
-        sessions_to_add = [Session(sets=[], id=session_id)]
+        sessions_to_add = [Session(id=session_id)]
         repository.add(sessions_to_add)
         session_fetched = repository.get([session_id])
         assert sessions_to_add == session_fetched
 
     def test_add_and_list(self):
         repository = CSVRepository()
-        session_id = uuid4()
-        sessions_to_add = [Session(sets=[], id=session_id)]
+        sessions_to_add = [Session()]
         repository.add(sessions_to_add)
         all_sessions = repository.list()
         assert sessions_to_add == all_sessions
+
+    def test_add_save_and_load(self, tmp_path):
+        # prep
+        path = tmp_path / "data.csv"
+        repository = CSVRepository(path)
+        sessions = [Session()]
+        # add
+        repository.add(sessions)
+        # save
+        repository.save()
+        # load
+        new_repository_instance = CSVRepository(path)
+        loaded_sessions = new_repository_instance.list()
+        assert sessions == loaded_sessions
