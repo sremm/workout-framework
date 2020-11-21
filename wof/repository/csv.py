a3ad4c3b79f834eb8a3424f1f4a2035a5c602253
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-21 17:18:19 +0100

    more logic for loading, empty method for saving

--- wof/repository/csv.py
+++ wof/repository/csv.py
@@ -8,10 +8,30 @@ from wof.repository.base import BaseRepository
 
 
 class CSVRepository(BaseRepository):
+    _columns = [
+        "date",
+        "session_id",
+        "exercise_name",
+        "exercise_id",
+        "reps",
+        "weight",
+        "set_number",
+        "unit",
+    ]
+
     def __init__(self, path: Union[Path, None] = None) -> None:
         self._data_path = path
-        self._raw_data = pd.read_csv(path) if path is not None else pd.DataFrame()
+        if path is None or path.is_file() == False:
+            self._raw_data = self._create_empty_data()
+        else:
+            try:
+                self._raw_data = pd.read_csv(path)
+            except pd.errors.EmptyDataError:
+                self._raw_data = self._create_empty_data()
         self._in_memory_data: Dict[UUID, Session] = {}
+
+    def _create_empty_data(self) -> pd.DataFrame:
+        return pd.DataFrame(columns=self._columns)
 
     def add(self, sessions: List[Session]) -> None:
         session_by_id = {x.id: x for x in sessions}
@@ -22,3 +42,6 @@ class CSVRepository(BaseRepository):
 
     def list(self) -> List[Session]:
         return list(self._in_memory_data.values())
+
+    def save(self) -> None:
+        pass
