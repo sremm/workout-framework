03be2509bbd22dc1c49c72408def58be3fde6ac2
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-21 17:47:31 +0100

    create some domain tests

--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -1,1 +1,18 @@
-from wof.domain import model
+from wof.domain.model import Session
+from wof.domain.model import Set
+from wof.domain.model import Excercise
+
+
+def test_add_sets_to_session():
+    session = Session()
+    sets = [Set(), Set()]
+    session.add_sets(sets)
+    assert len(session) == 2
+
+
+def test_create_set_with_multiple_excercises():
+    a_set = Set(
+        excercises=[Excercise("first"), Excercise("second")],
+    )
+
+    assert len(a_set) == 2
