96e8c2940178deac0463699834bc98f497598dd1
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-24 06:43:40 +0100

    update test allocate in batch

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,9 +1,10 @@
 from wof.repository.base import FakeRepository
 from wof import services
+from wof.domain.model import Session, WorkoutSet
 
 
 def test_allocate_in_batch():
     repo = FakeRepository()
-    sessions = []
+    sessions = [Session([WorkoutSet()])]
     services.allocate_in_batch(sessions, repo)
-    assert 1
+    assert len(repo.list()) == 1
