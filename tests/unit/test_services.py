d0d861cc97c9ddecd2419dff293b45cf2315d183
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-24 07:06:23 +0100

    remove WorkoutSet dependency from test

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,11 +1,11 @@
 from wof.repository.base import FakeRepository
 from wof.service_layer import services
-from wof.domain.model import Session, WorkoutSet
+from wof.domain.model import Session
 
 
 def test_allocate_in_batch():
     repo = FakeRepository()
-    sessions = [Session([WorkoutSet()])]
+    sessions = [Session()]
     services.allocate_in_batch(sessions, repo)
     assert len(repo.list()) == 1
     assert repo.committed == True
