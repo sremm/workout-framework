276ffb4d6512570588fddf31d24804836414e1db
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-27 07:47:21 +0100

    add default csv database path

--- wof/config.py
+++ wof/config.py
@@ -1,2 +1,8 @@
+from pathlib import Path
+
 BACKEND_IP = "localhost"
 BACKEND_PORT = 8000
+
+
+def get_csv_database_path():
+    return Path(__file__).parent.parent / "data" / "csv_dataset" / "data.csv"
