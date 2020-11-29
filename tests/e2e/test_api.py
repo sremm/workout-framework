739361e8f238e986dbbb27356bd955213a930494
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-29 08:16:05 +0100

    rename fast_api_app.py to main.py

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 from fastapi.testclient import TestClient
-from wof.entrypoints.fast_api_app import app
+from wof.entrypoints.main import app
 
 
 @pytest.fixture(scope="module")
@@ -13,7 +13,7 @@ def test_app(tmp_path_factory):
     os.environ.pop("CSV_DATASET_PATH")
 
 
-def test_get_workout_sessions(test_app):
+def test_happy_path_add_session_add_sets_get_len(test_app):
     with test_app as client:  # this runs startup event
         response = client.get("/workout_sessions")
         assert response.status_code == 200
