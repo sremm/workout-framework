5a9056a2c6441200b85d020e3982fef8ed0e44f2
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-29 12:07:22 +0100

    add stuff to happy path test

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,4 +1,6 @@
 import os
+from typing import Dict
+
 import pytest
 from fastapi.testclient import TestClient
 from wof.entrypoints.main import app
@@ -18,16 +20,35 @@ def test_happy_path_add_session_add_sets_get_len(test_app):
     with test_app as client:  # this runs startup event
         # add an empty session
         workout_session_id = "123"
-        response = client.post(f"/workout_sessions/{workout_session_id}")
+        response = client.post(
+            f"/workout_sessions/{workout_session_id}",
+        )
         assert response.status_code == 200
         assert response.json() == {"workout_session_id": workout_session_id}
 
-        response = client.get("/workout_sessions")
-        assert response.status_code == 200
-        assert response.json() == {"number_of_sessions": 1}
-
-        response = client.put(f"/sets/{workout_session_id}")
+        set_data: Dict = {
+            "exercise": "name",
+            "reps": 1,
+            "weights": 10.0,
+            "set_number": 1,
+            "unit": "kg",
+        }
+        response = client.put(
+            f"/sets/{workout_session_id}",
+            json=[set_data],
+        )
         assert response.status_code == 200
         assert response.json() == {
             "msg": f"1 set(s) added to workout session {workout_session_id}"
         }
+
+        response = client.get("/workout_sessions")
+        results = response.json()
+        del results[0]["date_time"]
+        assert response.status_code == 200
+        assert results == [
+            {
+                "sets": [set_data],
+                "id": workout_session_id,
+            }
+        ]
