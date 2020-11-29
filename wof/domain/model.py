0c633e36fa1f0afbff9774abe28b415564ac5012
Sten Remmelg <sten.remmelg@gmail.com>
2020-11-29 11:53:32 +0100

    convert model classes to pydantic basemodel

--- wof/domain/model.py
+++ wof/domain/model.py
@@ -1,13 +1,11 @@
-from dataclasses import field
-from pydantic.dataclasses import dataclass
+from pydantic import BaseModel, Field
 from typing import Union, List
 
 from datetime import datetime
 from uuid import uuid4
 
 
-@dataclass
-class Exercise:
+class Exercise(BaseModel):
     """
     Descriptor for an exercise
     Has a unique id, but as with good childern, can have many names
@@ -17,15 +15,14 @@ class Exercise:
     names: List[str]
 
 
-@dataclass
-class WorkoutSet:
+class WorkoutSet(BaseModel):
     """ Class to define a workout set """
 
     exercise: Union[str, List[str]] = "name"
     reps: Union[int, List[int]] = 0
     weights: Union[float, List[float]] = 0
     unit: str = "kg"
-    set_number: int = 0  # used in Intensity app as "Set"
+    set_number: int = 1  # used in Intensity app as "Set"
 
     def __len__(self) -> int:
         if type(self.exercise) == list:
@@ -40,13 +37,12 @@ def uuid4_as_str(*args) -> str:
     return str(uuid4(*args))
 
 
-@dataclass
-class WorkoutSession:
+class WorkoutSession(BaseModel):
     """ Class for keeping track of session data """
 
-    sets: List[WorkoutSet] = field(default_factory=list)
-    id: str = field(default_factory=uuid4_as_str)
-    date_time: datetime = field(default_factory=datetime.now)
+    sets: List[WorkoutSet] = Field(default_factory=list)
+    id: str = Field(default_factory=uuid4_as_str)
+    date_time: datetime = Field(default_factory=datetime.now)
 
     def add_sets(self, sets: List[WorkoutSet]) -> None:
         self.sets.extend(sets)
