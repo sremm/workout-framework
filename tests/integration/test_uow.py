21a334876290aeb362957378014ab9076a213bcb
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-01 08:22:09 +0100

    add tests to confirm rollback behaviour

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -1,12 +1,15 @@
 from wof.service_layer.unit_of_work import CSVUnitOfWork
 from wof.adapters.csv import csv_session_factory
 from wof.domain import model
+import pytest
 
 
-def test_uow_can_add_workout_sessions(tmp_path):
-    dataset_path = tmp_path / "test_data.csv"
-    session_factory = csv_session_factory(dataset_path)
+@pytest.fixture
+def session_factory(tmp_path):
+    return csv_session_factory(tmp_path / "test_data.csv")
 
+
+def test_uow_can_add_workout_sessions(session_factory):
     uow = CSVUnitOfWork(session_factory)
     with uow:
         workout_sessions = [
@@ -16,3 +19,33 @@ def test_uow_can_add_workout_sessions(tmp_path):
         uow.commit()
     fetched_session = uow.repo.get(["abc123"])
     assert fetched_session == workout_sessions
+
+
+def test_rolls_back_uncommited_work_by_default(session_factory):
+    uow = CSVUnitOfWork(session_factory)
+    with uow:
+        workout_sessions = [
+            model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
+        ]
+        uow.repo.add(workout_sessions)
+    session_list = uow.repo.list()
+    assert session_list == []
+
+
+def test_rolls_back_on_exception(session_factory):
+    class MyException(Exception):
+        pass
+
+    uow = CSVUnitOfWork(session_factory)
+    try:
+        with uow:
+            workout_sessions = [
+                model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
+            ]
+            uow.repo.add(workout_sessions)
+            raise MyException()
+    except MyException:
+        pass
+
+    session_list = uow.repo.list()
+    assert session_list == []
