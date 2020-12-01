3ce05fc29b29d71d2e7060a5d633f0407345c641
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-01 08:05:11 +0100

    make services use uow

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -2,23 +2,14 @@ from typing import List
 
 from wof.adapters import repository
 from wof.domain.model import WorkoutSession, WorkoutSet
-from wof.service_layer import services
-
-
-class FakeSession:
-    committed = False
-
-    def commit(self):
-        self.committed = True
+from wof.service_layer import services, unit_of_work
 
 
 class FakeRepository(repository.BaseRepository):
-    def __init__(self, session) -> None:
-        self.session = session
+    def __init__(self) -> None:
         self._data: List[WorkoutSession] = []
 
     def add(self, sessions: List[WorkoutSession]) -> None:
-        self.session.committed = False
         return self._data.extend(sessions)
 
     def get(self, ids: List[str]) -> List[WorkoutSession]:
@@ -28,28 +19,41 @@ class FakeRepository(repository.BaseRepository):
         return list(self._data)
 
 
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+    def __init__(self) -> None:
+        self.repo = FakeRepository()
+        self.committed = False
+
+    def __enter__(self):
+        pass
+
+    def commit(self):
+        self.committed = True
+
+    def rollback(self):
+        pass
+
+
 def test_add_workout_sessions():
-    db_session = FakeSession()
-    repo = FakeRepository(db_session)
+    uow = FakeUnitOfWork()
     sessions = [WorkoutSession()]
-    services.add_workout_sessions(sessions, repo, db_session)
-    assert len(repo.list()) == 1
-    assert db_session.committed == True
+    services.add_workout_sessions(sessions, uow)
+    assert len(uow.repo.list()) == 1
+    assert uow.committed == True
 
 
 def test_add_set_to_existing_session():
-    db_session = FakeSession()
-    repo = FakeRepository(db_session)
+    uow = FakeUnitOfWork()
     session_id = "123"
     sessions = [WorkoutSession(id=session_id)]
-    services.add_workout_sessions(sessions, repo, db_session)
+    services.add_workout_sessions(sessions, uow)
 
     sets = [
         WorkoutSet(exercise="name", reps=1, weights=0, set_number=1),
         WorkoutSet(exercise="name", reps=1, weights=0, set_number=2),
     ]
-    services.add_sets_to_workout_session(sets, session_id, repo, db_session)
+    services.add_sets_to_workout_session(sets, session_id, uow)
 
-    fetched_session = repo.get([session_id])[0]
+    fetched_session = uow.repo.get([session_id])[0]
     number_of_sets = len(fetched_session)
     assert number_of_sets == 2
--- wof/service_layer/services.py
+++ wof/service_layer/services.py
@@ -6,10 +6,11 @@ from wof.adapters.repository import BaseRepository
 
 
 def add_workout_sessions(
-    sessions: List[WorkoutSession], repo: BaseRepository, db_session
+    sessions: List[WorkoutSession], uow: AbstractUnitOfWork
 ) -> None:
-    repo.add(sessions)
-    db_session.commit()
+    with uow:
+        uow.repo.add(sessions)
+        uow.commit()
 
 
 class InvalidSessionId(Exception):
@@ -17,28 +18,24 @@ class InvalidSessionId(Exception):
 
 
 def add_sets_to_workout_session(
-    sets: List[WorkoutSet], session_id: str, repo: BaseRepository, db_session
+    sets: List[WorkoutSet], session_id: str, uow: AbstractUnitOfWork
 ):
-    workout_sessions = repo.get([session_id])
-    if len(workout_sessions) == 1:
-        workout_session = workout_sessions[0]
-        workout_session.add_sets(sets)
-        db_session.commit()
-        return sets
-    else:
-        if len(workout_sessions) == 0:
-            raise InvalidSessionId(f"Found no workout sessions with {session_id=}")
+    with uow:
+        workout_sessions = uow.repo.get([session_id])
+        if len(workout_sessions) == 1:
+            workout_session = workout_sessions[0]
+            workout_session.add_sets(sets)
+            uow.commit()
+            return sets
         else:
-            raise Exception(
-                f"Found {len(workout_sessions)} sessions with {session_id=}, but should only get one"
-            )
+            if len(workout_sessions) == 0:
+                raise InvalidSessionId(f"Found no workout sessions with {session_id=}")
+            else:
+                raise Exception(
+                    f"Found {len(workout_sessions)} sessions with {session_id=}, but should only get one"
+                )
 
 
-def list_all_sessions(repo: BaseRepository) -> List[WorkoutSession]:
-    return repo.list()
-
-
-# def add_workout_sessions(sessions: List[WorkoutSession], uow: AbstractUnitOfWork):
-#     with uow:
-#         uow.repo.add(sessions)
-#         uow.commit()
+def list_all_sessions(uow: AbstractUnitOfWork) -> List[WorkoutSession]:
+    with uow:
+        return uow.repo.list()
