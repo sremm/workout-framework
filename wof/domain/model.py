e478f41739e985f9e5934d6840fe25b53519b399
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-02 09:43:26 +0100

    start with ideas about a more intrecate WorkoutSession

--- wof/domain/model.py
+++ wof/domain/model.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel, Field
-from typing import Union, List
+""" Domain data models 
 
+Series x Sets x Reps of Excercies
+
+"""
 from datetime import datetime
+from typing import List, Union
 from uuid import uuid4
+
+from pydantic import BaseModel, Field
 
 
 class Exercise(BaseModel):
@@ -22,7 +27,8 @@ class WorkoutSet(BaseModel):
     reps: Union[int, List[int]] = 0
     weights: Union[float, List[float]] = 0
     unit: str = "kg"
-    set_number: int = 1  # used in Intensity app as "Set"
+    set_number: int = 1  # used in Intensity app as "Set" # would be nice to instead have start time and length later but optional
+    # order of sets could be inferred if we have start times, but for imported data we probably don't
 
     def __len__(self) -> int:
         if type(self.exercise) == list:
@@ -43,9 +49,23 @@ class WorkoutSession(BaseModel):
     sets: List[WorkoutSet] = Field(default_factory=list)
     id: str = Field(default_factory=uuid4_as_str)
     date_time: datetime = Field(default_factory=datetime.now)
+    # heart_rate: HeartRateSignal # could have HR signal here
+    # sections: BaseSection # Could have sections instead sets here
 
     def add_sets(self, sets: List[WorkoutSet]) -> None:
         self.sets.extend(sets)
 
     def __len__(self) -> int:
         return len(self.sets)
+
+
+# might not quite work to validate and reconstuct, don't know yet
+class BaseSection(BaseModel):
+    """ A baseclass for Workout Section, subclass to create a specific section """
+
+    start_time: datetime
+    end_time: datetime  # or length ? either way both can be calculated from the other
+
+
+class StrengthSection(BaseSection):
+    sets: List[WorkoutSet]
