5ed86a5ab7202b56c54cbb4f136499346e776528
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-04 10:06:22 +0100

    implement looping trough polar sessions

--- wof/import_logic/polar.py
+++ wof/import_logic/polar.py
@@ -1,8 +1,18 @@
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
-from wof.domain import model
+from wof.domain.model import WorkoutSession
+
+import json
 
 
-def load_all_sessions_in_folder(path: Path) -> List[model.WorkoutSession]:
-    return []
+def load_all_sessions_in_folder(path: Path) -> List[WorkoutSession]:
+    def _convert_to_workout_session(data: Dict) -> WorkoutSession:
+        return WorkoutSession()
+
+    results = []
+    for session_file_path in path.glob("training-session*.json"):
+        with session_file_path.open("r") as f:
+            data = json.load(f)
+        results.append(_convert_to_workout_session(data))
+    return results
