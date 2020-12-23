a3943c0e4b92adf2f37a82f113944a33e7e62df7
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-23 18:51:55 +0200

    prepare for MongoUoW

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -5,47 +5,46 @@ import pytest
 
 
 @pytest.fixture
-def session_factory(tmp_path):
+def csv_session_factory_instance(tmp_path):
     return csv_session_factory(tmp_path / "test_data.csv")
 
 
-def test_uow_can_add_workout_sessions(session_factory):
-    uow = CSVUnitOfWork(session_factory)
-    with uow:
-        workout_sessions = [
-            model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
-        ]
-        uow.repo.add(workout_sessions)
-        uow.commit()
-    fetched_session = uow.repo.get(["abc123"])
-    assert fetched_session == workout_sessions
-
-
-def test_rolls_back_uncommited_work_by_default(session_factory):
-    uow = CSVUnitOfWork(session_factory)
-    with uow:
-        workout_sessions = [
-            model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
-        ]
-        uow.repo.add(workout_sessions)
-    session_list = uow.repo.list()
-    assert session_list == []
-
-
-def test_rolls_back_on_exception(session_factory):
-    class MyException(Exception):
-        pass
-
-    uow = CSVUnitOfWork(session_factory)
-    try:
+class TestCSVUoW:
+    def test_uow_can_add_workout_sessions(self, csv_session_factory_instance):
+        uow = CSVUnitOfWork(csv_session_factory_instance)
         with uow:
             workout_sessions = [
                 model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
             ]
             uow.repo.add(workout_sessions)
-            raise MyException()
-    except MyException:
-        pass
+            uow.commit()
+        fetched_session = uow.repo.get(["abc123"])
+        assert fetched_session == workout_sessions
 
-    session_list = uow.repo.list()
-    assert session_list == []
+    def test_rolls_back_uncommited_work_by_default(self, csv_session_factory_instance):
+        uow = CSVUnitOfWork(csv_session_factory_instance)
+        with uow:
+            workout_sessions = [
+                model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
+            ]
+            uow.repo.add(workout_sessions)
+        session_list = uow.repo.list()
+        assert session_list == []
+
+    def test_rolls_back_on_exception(self, csv_session_factory_instance):
+        class MyException(Exception):
+            pass
+
+        uow = CSVUnitOfWork(csv_session_factory_instance)
+        try:
+            with uow:
+                workout_sessions = [
+                    model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
+                ]
+                uow.repo.add(workout_sessions)
+                raise MyException()
+        except MyException:
+            pass
+
+        session_list = uow.repo.list()
+        assert session_list == []
