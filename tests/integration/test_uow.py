9e9b547f11bb4ac5419745d2e245362bf62c9bcf
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-25 08:49:46 +0200

    start implementing tests for MongoUoW

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -1,7 +1,54 @@
-from wof.service_layer.unit_of_work import CSVUnitOfWork
+from wof.service_layer.unit_of_work import CSVUnitOfWork, MongoUnitOfWork
+import os
+from wof.adapters.mongo_db import MongoSettings, mongo_session_factory
+from pymongo import MongoClient
 from wof.adapters.csv import csv_session_factory
 from wof.domain import model
 import pytest
+from datetime import datetime
+
+
+@pytest.fixture
+def mongo_session_factory_instance():
+    # set env vars for MongoSettings
+    evars = {
+        "MONGO_DATABASE": "test_db",
+        "MONGO_PORT": "27017",
+        "MONGO_HOST": "localhost",
+    }
+    for key, val in evars.items():
+        os.environ[key] = val
+    yield mongo_session_factory(MongoSettings())
+    # clear test database
+    mongo_settings = MongoSettings()
+    client = MongoClient(mongo_settings.mongo_host, mongo_settings.mongo_port)
+    collection = client[mongo_settings.mongo_database][mongo_settings.main_collection]
+    collection.drop()
+    # remove environment variables
+    for key, val in evars.items():
+        os.environ.pop(key)
+
+
+class TestMongoUoW:
+    def test_uow_can_add_workout_sessions(self, mongo_session_factory_instance):
+        uow = MongoUnitOfWork(mongo_session_factory_instance)
+        with uow:
+            workout_sessions = [
+                model.WorkoutSession(id="abc123", sets=[model.WorkoutSet()])
+            ]
+            workout_sessions[0].start_time = datetime.strptime(
+                str(workout_sessions[0].start_time)[:-3], "%Y-%m-%d %H:%M:%S.%f"
+            )  # since mongodb truncates to milliseconds from microseconds
+            added_session_ids = uow.repo.add(workout_sessions)
+            uow.commit()
+        fetched_session = uow.repo.get(added_session_ids)
+        assert fetched_session == workout_sessions
+
+    def test_rolls_back_uncommited_work_by_default(self):
+        assert 0
+
+    def test_rolls_back_on_exception(self):
+        assert 0
 
 
 @pytest.fixture
