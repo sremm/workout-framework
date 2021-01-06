6c23908aed90a5b3479097acad433c3c9072ac68
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-06 15:04:20 +0100

    add a test for concurrent updates not being allowed right now

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import threading
 from typing import List
 
 import pytest
@@ -8,6 +9,8 @@ from wof.adapters.csv import csv_session_factory
 from wof.adapters.mongo_db import MongoSettings, mongo_session_factory
 from wof.domain import model
 from wof.service_layer.unit_of_work import CSVUnitOfWork, MongoUnitOfWork
+import time
+import traceback
 
 
 @pytest.fixture
@@ -31,7 +34,55 @@ def mongo_session_factory_instance():
         os.environ.pop(key)
 
 
+def slow_update(
+    session_factory, session_id, new_sets: List[model.WorkoutSet], exceptions: List
+):
+
+    try:
+        uow = MongoUnitOfWork(session_factory)
+        with uow:
+            uow.repo.update(session_id, new_sets)
+            time.sleep(0.2)
+            uow.commit()
+    except Exception as e:
+        print(traceback.format_exc())
+        exceptions.append(e)
+
+
 class TestMongoUoW:
+    def test_concurrent_updates_to_version_are_not_allowed(
+        self, mongo_session_factory_instance
+    ):
+        uow = MongoUnitOfWork(mongo_session_factory_instance)
+        with uow:
+            workout_sessions = [model.WorkoutSession(sets=[model.WorkoutSet()])]
+            added_session_ids = uow.repo.add(workout_sessions)
+            uow.commit()
+        new_sets_1, new_sets_2 = (
+            [model.WorkoutSet(exercise="exercise_1")],
+            [model.WorkoutSet(exercise="exercise_2")],
+        )
+        exceptions = []
+        try_to_update_session_1 = lambda: slow_update(
+            mongo_session_factory_instance, added_session_ids[0], new_sets_1, exceptions
+        )
+        try_to_update_session_2 = lambda: slow_update(
+            mongo_session_factory_instance, added_session_ids[0], new_sets_2, exceptions
+        )
+
+        thread1 = threading.Thread(target=try_to_update_session_1)
+        thread2 = threading.Thread(target=try_to_update_session_2)
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread2.join()
+
+        with uow:
+            workout_session = uow.repo.get(added_session_ids)[0]
+
+        assert workout_session.version == 2
+        assert len(workout_session) == 2
+
     def test_uow_can_add_and_update_workout_sessions(
         self, mongo_session_factory_instance
     ):
