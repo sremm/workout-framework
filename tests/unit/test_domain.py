2e4dfe1d0f0eaca9f17a7e80e61d93cf639e257a
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-09 11:26:57 +0100

    add test for events in domain

--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from wof.domain.model import TimeSeries, WorkoutSession
 from wof.domain.model import WorkoutSet
+from wof.domain import events
 from typing import List
 
 
@@ -30,3 +31,13 @@ def test_create_session_with_hr_data():
     session.update_heart_rate(hr_data)
 
     assert session.heart_rate == hr_data
+
+
+def test_event_raised_when_adding_many_sets_to_a_workout_session():
+    session = WorkoutSession()
+    sets = [WorkoutSet() for _ in range(10)]
+    session.add_sets(sets)
+    assert len(session.events) == 1, "Events empty but should have 1 etry"
+    assert session.events[-1] == events.ManySetsAddedToWorkoutSession(
+        workout_session_id=session.id, number_of_sets_added=10
+    )
