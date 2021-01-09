1bfb9731ba8c6095e5baac8dd82a0fa730f122f3
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-09 14:10:16 +0100

    implement printout in event handler

--- wof/service_layer/messagebus.py
+++ wof/service_layer/messagebus.py
@@ -2,13 +2,20 @@ from wof.domain import events
 from typing import Callable, Dict, List
 
 
-def send_many_sets_added_notification():
-    pass
+def send_many_sets_added_notification(event: events.ManySetsAddedToWorkoutSession):
+    print(
+        f"Many sets ({event.number_of_sets_added}) were added to session with id: {event.id}"
+    )
 
 
 HANDLERS: Dict[events.Event, List[Callable]] = {
     events.ManySetsAddedToWorkoutSession: [send_many_sets_added_notification]
 }
+
+
+def handle_many(events: List[events.Event]):
+    for event in events:
+        handle(event)
 
 
 def handle(event: events.Event):
