41eae282b3461cd7fdabe2969df1bb4710deeaed
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-15 07:52:07 +0100

    add a multi excercise set to analytics test

--- tests/unit/test_analytics.py
+++ tests/unit/test_analytics.py
@@ -14,6 +14,10 @@ from wof.domain.model import (
 def test_workout_session_summary_from_one_session():
     sets = [
         WorkoutSet(exercise="one", reps=5, weight=10, set_number=x) for x in range(1, 4)
+    ] + [
+        WorkoutSet(
+            excercise=["two", "three"], reps=[5, 5], weight=[10, 10], set_number=4
+        )
     ]
     time: List[datetime] = [
         datetime(2000, 1, 1, 15, 0, 0, 0),
@@ -25,7 +29,7 @@ def test_workout_session_summary_from_one_session():
     summary = compute_workout_sessions_summary(workout_sessions)
     assert summary == WorkoutSessionsSummary(
         session_ids=[workout_sessions[0].id],
-        total_reps=15,
-        total_weight_lifted=150.0,
+        total_reps=25,
+        total_weight_lifted=170.0,
         heart_rate_stats=TimeSeriesStats(mean=141.0, min=140.0, max=141.0, std=1.0),
     )
