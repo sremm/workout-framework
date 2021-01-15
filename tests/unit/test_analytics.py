bce2db5e5bd10705c3854d7ec5c70fc06d9a329c
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-15 09:01:00 +0100

    simplify analytics test

--- tests/unit/test_analytics.py
+++ tests/unit/test_analytics.py
@@ -8,16 +8,13 @@ from wof.domain.model import (
     WorkoutSession,
     WorkoutSessionsSummary,
     WorkoutSet,
+    WorkoutSetStats,
 )
 
 
 def test_workout_session_summary_from_one_session():
     sets = [
         WorkoutSet(exercise="one", reps=5, weight=10, set_number=x) for x in range(1, 4)
-    ] + [
-        WorkoutSet(
-            excercise=["two", "three"], reps=[5, 5], weight=[10, 10], set_number=4
-        )
     ]
     time: List[datetime] = [
         datetime(2000, 1, 1, 15, 0, 0, 0),
@@ -29,7 +26,8 @@ def test_workout_session_summary_from_one_session():
     summary = compute_workout_sessions_summary(workout_sessions)
     assert summary == WorkoutSessionsSummary(
         session_ids=[workout_sessions[0].id],
-        total_reps=25,
-        total_weight_lifted=170.0,
+        workout_set_stats=WorkoutSetStats(
+            total_reps=15, total_weight_lifted=150.0, exercise=("one",)
+        ),
         heart_rate_stats=TimeSeriesStats(mean=141.0, min=140.0, max=141.0, std=1.0),
     )
--- wof/domain/analytics.py
+++ wof/domain/analytics.py
@@ -2,8 +2,28 @@ from wof.domain import model
 from typing import List
 
 
+def compute_time_series_stats(data: List[model.TimeSeries]) -> model.TimeSeriesStats:
+
+    return model.TimeSeriesStats(mean=1, min=2, max=3, std=1.0)
+
+
+def compute_merged_set_stats(
+    sessions: List[model.WorkoutSession],
+) -> model.WorkoutSetStats:
+    stats: List[model.WorkoutSetStats] = [
+        model.WorkoutSetStats.calculate(session.sets) for session in sessions
+    ]
+    result: model.WorkoutSetStats = sum(stats)
+    return result
+
+
 def compute_workout_sessions_summary(
     sessions: List[model.WorkoutSession],
 ) -> model.WorkoutSessionsSummary:
-    # do calculations
-    return model.WorkoutSessionsSummary()
+    return model.WorkoutSessionsSummary(
+        session_ids=[x.id for x in sessions],
+        workout_set_stats=compute_merged_set_stats(sessions),
+        heart_rate_stats=compute_time_series_stats(
+            [x.heart_rate for x in sessions if x.heart_rate is not None]
+        ),
+    )
