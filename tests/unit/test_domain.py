8c5ebf92402d3fed3b1bf5a3a3c6fe60c0d96b7b
sremm <33256557+sremm@users.noreply.github.com>
2021-01-17 21:05:33 +0100

    Impelment Session Summary and underlying statistics functionality (#3)
    * remove misleading comment
    
    * define workout session summary v1
    
    * start defining some analytics and tests for it
    
    * finalise first test on analytics
    
    * add property to set
    
    * add a multi excercise set to analytics test
    
    * implement addition for workout set stats
    
    * simplify analytics test
    
    * start creating static method for workout set stats to calculate from workout sets
    
    * implement computation of workout set stats
    
    * rename method and make it public since it is usefult to be able to sum stats elsewhere as well
    
    * implement computing stats for single time series
    
    * add property for easy access to values as array
    
    * update analytics tests
    
    * implement some analytics
    
    * put all stats stuff to analytics module
    
    * sort imports

--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -1,43 +1,43 @@
 from datetime import datetime
-from wof.domain.model import TimeSeries, WorkoutSession
-from wof.domain.model import WorkoutSet
-from wof.domain import events
 from typing import List
 
-
-def test_add_sets_to_session():
-    session = WorkoutSession()
-    sets = [WorkoutSet(), WorkoutSet()]
-    session.add_sets(sets)
-    assert len(session) == 2
+from wof.domain import events
+from wof.domain.model import TimeSeries, WorkoutSession, WorkoutSet
 
 
-def test_create_set_with_multiple_exercises():
-    a_set = WorkoutSet(
-        exercise=["first", "second"],
-    )
-    assert len(a_set) == 2
+class TestWorkoutSet:
+    def test_create_with_multiple_exercises(self):
+        a_set = WorkoutSet(
+            exercise=["first", "second"],
+        )
+        assert len(a_set) == 2
 
 
-def test_create_session_with_hr_data():
-    session = WorkoutSession()
+class TestWorkoutSession:
+    def test_create_with_hr_data(self):
+        session = WorkoutSession()
 
-    time: List[datetime] = [
-        datetime(2000, 1, 1, 15, 0, 0, 0),
-        datetime(2000, 1, 1, 15, 0, 1, 0),
-        datetime(2000, 1, 1, 15, 0, 2, 0),
-    ]
-    hr_data = TimeSeries(values=[1, 2, 3], time=time, unit="bpm")
-    session.update_heart_rate(hr_data)
+        time: List[datetime] = [
+            datetime(2000, 1, 1, 15, 0, 0, 0),
+            datetime(2000, 1, 1, 15, 0, 1, 0),
+            datetime(2000, 1, 1, 15, 0, 2, 0),
+        ]
+        hr_data = TimeSeries(values=[1, 2, 3], time=time, unit="bpm")
+        session.update_heart_rate(hr_data)
 
-    assert session.heart_rate == hr_data
+        assert session.heart_rate == hr_data
 
+    def test_add_sets(self):
+        session = WorkoutSession()
+        sets = [WorkoutSet(), WorkoutSet()]
+        session.add_sets(sets)
+        assert len(session) == 2
 
-def test_event_raised_when_adding_many_sets_to_a_workout_session():
-    session = WorkoutSession()
-    sets = [WorkoutSet() for _ in range(10)]
-    session.add_sets(sets)
-    assert len(session.events) == 1, "Events empty but should have 1 etry"
-    assert session.events[-1] == events.ManySetsAddedToWorkoutSession(
-        id=session.id, number_of_sets_added=10
-    )
+    def test_event_raised_when_adding_many_sets(self):
+        session = WorkoutSession()
+        sets = [WorkoutSet() for _ in range(10)]
+        session.add_sets(sets)
+        assert len(session.events) == 1, "Events empty but should have 1 etry"
+        assert session.events[-1] == events.ManySetsAddedToWorkoutSession(
+            id=session.id, number_of_sets_added=10
+        )
