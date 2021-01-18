c7aa4e710c4d40a34a6d8b50951c51c714872fbe
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-18 08:07:48 +0100

    add a more specific test just for merging

--- tests/unit/test_merging.py
+++ tests/unit/test_merging.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from wof.domain.model import TimeSeries, WorkoutSession, WorkoutSet
-from wof.import_logic.data_merging import merge_polar_and_instensity_imports
+from wof.import_logic.data_merging import (
+    merge_polar_and_instensity_imports,
+    merge_sessions,
+)
 
 
 def create_timeseries_entry() -> TimeSeries:
@@ -10,6 +13,32 @@ def create_timeseries_entry() -> TimeSeries:
         datetime(2000, 1, 1, 15, 0, 2, 0),
     ]
     return TimeSeries(values=[1, 2, 3], time=time, unit="bpm")
+
+
+class TestMergeSessions:
+    def test_with_timeseries_and_sets(self):
+        base_session = WorkoutSession(
+            id="sess1",
+            start_time=datetime(2020, 1, 1, 17, 0, 0, 0),
+            stop_time=datetime(2020, 1, 1, 18, 0, 0, 0),
+            heart_rate=create_timeseries_entry(),
+        )
+
+        sessions = [
+            WorkoutSession(
+                id="sess2",
+                sets=[WorkoutSet()],
+                start_time=datetime(2020, 1, 1),
+            ),
+        ]
+        result = merge_sessions(base_session, sessions_to_merge=sessions)
+        assert result == WorkoutSession(
+            id="sess1",
+            sets=[WorkoutSet()],
+            start_time=datetime(2020, 1, 1, 17, 0, 0, 0),
+            stop_time=datetime(2020, 1, 1, 18, 0, 0, 0),
+            heart_rate=create_timeseries_entry(),
+        )
 
 
 class TestPolarIntensityMerge:
