ce1d068d5968fa1ad7b501fa59d43189ae33e715
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-26 08:31:43 +0100

    create new events

--- wof/domain/events.py
+++ wof/domain/events.py
@@ -1,7 +1,32 @@
+from typing import List, Optional, Tuple
+
+import datetime
 from pydantic import BaseModel
+from wof.domain.model import WorkoutSet
 
 
 class Event(BaseModel):
+    pass
+
+
+class SessionStarted(Event):
+    pass
+
+
+class SetsCompleted(Event):
+    sets: List[WorkoutSet]
+
+
+class DateTimeRange(BaseModel):
+    start: datetime.date
+    end: datetime.date
+
+
+class SessionsRequested(Event):
+    date_range: Optional[DateTimeRange]
+
+
+class ImportRequested(Event):
     pass
 
 
