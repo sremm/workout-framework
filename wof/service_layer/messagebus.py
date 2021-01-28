1a637bd25e8f201a45ec03e0fb18ff40a719bb51
Sten Remmelg <sten.remmelg@gmail.com>
2021-01-28 08:29:06 +0100

    update message bus

--- wof/service_layer/messagebus.py
+++ wof/service_layer/messagebus.py
@@ -1,15 +1,22 @@
+from wof.service_layer.unit_of_work import AbstractUnitOfWork
 from wof.domain import events
+from wof.service_layer import handlers
 from typing import Callable, Dict, List
 
+Handler = Callable[events.Event, AbstractUnitOfWork]
 
-HANDLERS: Dict[events.Event, List[Callable]] = {}
+HANDLERS: Dict[events.Event, List[Handler]] = {
+    # events.SessionStarted: handlers...
+    events.SessionsToAdd: [handlers.add_workout_sessions],
+    events.SetsCompleted: [handlers.add_sets_to_workout_session],
+    events.SessionsRequested: [handlers.get_sessions],
+}
 
 
-def handle_many(events: List[events.Event]):
-    for event in events:
-        handle(event)
-
-
-def handle(event: events.Event):
-    for handler in HANDLERS[type(event)]:
-        handler(event)
+def handle(event: events.Event, uow: AbstractUnitOfWork):
+    queue = [event]
+    while queue:
+        event = queue.pop(0)
+        for handler in HANDLERS[type(event)]:
+            handler(event, uow)
+            queue.extend(uow.collect_new_events())
