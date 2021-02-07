2bfa52672c6d90d976b3dcb0cbd185ba4262c78e
Sten Remmelg <sten.remmelg@gmail.com>
2021-02-07 20:08:36 +0100

    create a bootstrap function to inject uow to message bus handle function

---
+++ wof/bootstrap.py
@@ -0,0 +1,14 @@
+from typing import Callable, List
+
+from wof.service_layer.messagebus import Message, handle
+from wof.service_layer import unit_of_work
+import functools
+
+HandleComposed = Callable[[Message], List]
+
+
+def bootstrap_handle(
+    uow: unit_of_work.AbstractUnitOfWork = unit_of_work.MongoUnitOfWork,
+) -> HandleComposed:
+    handle_composed = functools.partial(handle, uow=uow)
+    return handle_composed
