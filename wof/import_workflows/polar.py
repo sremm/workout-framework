059ae0e240af8ef54cafdf5335d3f725b6c75e38
Sten Remmelg <sten.remmelg@gmail.com>
2021-02-20 22:22:17 +0100

    add function to be able to convert polar from dicts

--- wof/import_workflows/polar.py
+++ wof/import_workflows/polar.py
@@ -10,41 +10,50 @@ class PolarFormatError(Exception):
     pass
 
 
-def confirm_export_format(data):
+def _confirm_export_format(data):
     if len(data["exercises"]) != 1:
         raise PolarFormatError(
             f"Expecting 1 entry in exercises, but got {len(data['exercises'])}"
         )
 
 
-def polar_date_conversion(date: str) -> datetime:
+def _polar_date_conversion(date: str) -> datetime:
     return datetime.strptime(date + "000", "%Y-%m-%dT%H:%M:%S.%f")
 
 
+def _convert_polar_samples(data: List[Dict], unit: str) -> TimeSeries:
+    values = []
+    time = []
+    for sample in data:
+        values.append(sample["value"])
+        time.append(_polar_date_conversion(sample["dateTime"]))
+    return TimeSeries(values=values, time=time, unit=unit)
+
+
+def _convert_to_workout_session(data: Dict) -> WorkoutSession:
+    _confirm_export_format(data)
+
+    start_time = _polar_date_conversion(data["startTime"])
+
+    heart_rate = _convert_polar_samples(
+        data["exercises"][0]["samples"]["heartRate"], unit="bpm"
+    )
+
+    return WorkoutSession(start_time=start_time, heart_rate=heart_rate)
+
+
 def load_all_sessions_in_folder(path: Path) -> List[WorkoutSession]:
-    def _convert_polar_samples(data: List[Dict], unit: str) -> TimeSeries:
-        values = []
-        time = []
-        for sample in data:
-            values.append(sample["value"])
-            time.append(polar_date_conversion(sample["dateTime"]))
-        return TimeSeries(values=values, time=time, unit=unit)
-
-    def _convert_to_workout_session(data: Dict) -> WorkoutSession:
-        confirm_export_format(data)
-
-        start_time = polar_date_conversion(data["startTime"])
-
-        heart_rate = _convert_polar_samples(
-            data["exercises"][0]["samples"]["heartRate"], unit="bpm"
-        )
-
-        return WorkoutSession(start_time=start_time, heart_rate=heart_rate)
-
     results = []
     for session_file_path in path.glob("training-session*.json"):
         with session_file_path.open("r") as f:
             data = json.load(f)
         results.append(_convert_to_workout_session(data))
     results.sort()
+    return results
+
+
+def load_all_sessions_from_dicts(data: List[Dict]) -> List[WorkoutSession]:
+    results = []
+    for session_data in data:
+        results.append(_convert_to_workout_session(session_data))
     return results
