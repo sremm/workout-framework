5f5964a8da4bbc3c1b6e02c4d9474bc9e668b0af
Sten Remmelg <sten.remmelg@gmail.com>
2021-02-21 21:28:53 +0100

    add option to upload both intensity and polar data in import_app and allow attaching debugger

--- wof/frontend/import_app.py
+++ wof/frontend/import_app.py
@@ -2,16 +2,26 @@ import requests
 import streamlit as st
 import config
 
+import ptvsd
+
+ptvsd.enable_attach(address=("localhost", 5678))
+# ptvsd.wait_for_attach()  # Only include this line if you always wan't to attach the debugger
+
 st.title("Data Importer")
 # define format selector
-export_format = st.selectbox("Export format", options=["Polar", "Intensity"])
+export_format = st.selectbox(
+    "Export format", options=["Polar", "Intensity", "PolarAndIntensity"]
+)
 # define uploader
 if export_format == "Intensity":
     uploader_text = "Select your Intensity export csv file"
-    uploader_kwargs = {"type": ["csv", "txt"]}
+    uploader_kwargs = {"type": ["csv"]}
 elif export_format == "Polar":
     uploader_text = "Select your Polar export json files"
     uploader_kwargs = {"type": ["json"], "accept_multiple_files": True}
+elif export_format == "PolarAndIntensity":
+    uploader_text = "Select your polar and intensity data"
+    uploader_kwargs = {"type": ["csv", "json"], "accept_multiple_files": True}
 else:
     uploader_text = "export format not supported"
     uploader_kwargs = {"type": [""]}
@@ -35,6 +45,22 @@ if st.button("Upload"):
             st.write("Starting upload")
             res = requests.post(
                 f"{config.get_api_url()}/import/polar",
+                files=files,
+            )
+            st.write(res)
+        elif export_format == "PolarAndIntensity":
+            # all json files as polar_files
+            polar_files = [("polar_files", x) for x in uploaded if ".json" in x.name]
+            # the csv file as intensity_file
+            intensity_files = [
+                ("intensity_file", x) for x in uploaded if ".csv" in x.name
+            ]
+            assert len(polar_files) >= 1, "No polar files .json found"
+            assert len(intensity_files) == 1, "Found more than 1 or 0 intensity files"
+            files = [*polar_files, *intensity_files]
+            st.write("Starting upload")
+            res = requests.post(
+                f"{config.get_api_url()}/import/polar_and_intensity_with_merge",
                 files=files,
             )
             st.write(res)
