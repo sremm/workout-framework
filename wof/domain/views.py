601e9c235ef36ffc230e8095f73c51899b58be03
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-05 22:10:53 +0100

    add handler for workout session summary

--- wof/domain/views.py
+++ wof/domain/views.py
@@ -1,6 +1,8 @@
 from typing import DefaultDict, Dict, List
 
 from wof.domain import commands
+from wof.domain.analytics import WorkoutSessionsSummary, compute_workout_sessions_summary
+from wof.domain.model import WorkoutSession
 from wof.service_layer.unit_of_work import AbstractUnitOfWork
 
 
@@ -15,4 +17,12 @@ def workout_sessions(command: commands.GetSessions, uow: AbstractUnitOfWork) -> 
 
     with uow:
         results = uow.db_session.find(_get_query_args())
+    return results
+
+
+def workout_sessions_summary(
+    command: commands.GetWorkoutSessionSummary, uow: AbstractUnitOfWork
+) -> WorkoutSessionsSummary:
+    sessions_dicts = workout_sessions(commands.GetSessions(date_range=command.date_range), uow)
+    results = compute_workout_sessions_summary([WorkoutSession(**data) for data in sessions_dicts])
     return results
