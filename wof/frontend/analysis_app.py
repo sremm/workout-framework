c9691ea76aba6d065bb648ca046304b1fa4385d9
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-06 23:51:04 +0100

    add date range to request

--- wof/frontend/analysis_app.py
+++ wof/frontend/analysis_app.py
@@ -1,25 +1,42 @@
+from datetime import date, datetime
 from typing import Dict
 
+import config
+
+# import ptvsd
 import requests
 import streamlit as st
+
+# ptvsd.enable_attach(address=("localhost", 5678))
+# # ptvsd.wait_for_attach()  # Only include this line if you always wan't to attach the debugger
+
+
+def convert_to_datetime(date: date) -> datetime:
+    return datetime(date.year, date.month, date.day)
+
 
 st.set_page_config(layout="wide")
 st.title("Workout Analysis")
 
 # Date selector
 col1, col2 = st.beta_columns(2)
-start_date = col1.date_input("Start date")
-end_date = col2.date_input("End date")
+start_date = convert_to_datetime(col1.date_input("Start date"))
+end_date = convert_to_datetime(col2.date_input("End date"))
 
 
 @st.cache
 def get_summary(start_date, end_date) -> Dict:
+    print(start_date)
+    print(end_date)
     print("fetcing data")
-    # response = requests.get("analytics/workout_sessions_summary")
-    # parser response
-    result = {"start": str(start_date), "end": str(end_date)}
-    return result
+    res = requests.get(
+        f"{config.get_api_url()}/analytics/workout_session_summary",
+        params=(("start", str(start_date)), ("end", end_date)),
+    )
+    return res.json()
 
 
-summary = get_summary(start_date, end_date)
+summary = {}
+if st.button("Fetch"):
+    summary = get_summary(start_date, end_date)
 st.write(summary)
