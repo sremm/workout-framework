e5f434bfb99ae9c87a81797da56eeccbeac8195f
Sten Remmelg <sten.remmelg@qualisys.se>
2021-07-15 18:23:38 +0200

    upgrade to grid

--- wof/frontend/analysis_app.py
+++ wof/frontend/analysis_app.py
@@ -116,7 +116,7 @@ if sessions_in_range != []:
 st.markdown("---")
 
 
-def strenght_training_sessions_view(sessions: List[model.WorkoutSession], desired_excercises: List[str]):
+def strenght_training_sessions_view(sessions: List[model.WorkoutSession], desired_excercises: List[str], max_cols):
     """ filters out non strenght training sessions and shows interesting information about the desired excercises sessions """
     st.write("Strenght training sessions")
     strength_sessions = [x for x in sessions if x.type.name == "Strength training"]
@@ -131,9 +131,16 @@ def strenght_training_sessions_view(sessions: List[model.WorkoutSession], desire
             if per_exercise != {}:
                 for excercise_name, sets in per_exercise.items():
                     sets_per_session.append((sess.start_time, excercise_name, sets))
-    cols = st.beta_columns(len(sets_per_session))
+
+    num_items = len(sets_per_session)
+    num_rows = num_items // max_cols + 1
+    num_cols = num_items if num_items <= max_cols else max_cols
+    grid = [st.beta_columns(num_cols) for _ in range(num_rows)]
     for idx, (start_time, name, sets) in enumerate(sets_per_session):
-        col = cols[idx]
+        cur_row = abs((num_cols - (idx + 1)) // num_cols)
+        cur_col = idx - num_cols * cur_row
+        col = grid[cur_row][cur_col]
+
         col.write(f"Date: {start_time}")
         iso_cal = start_time.isocalendar()
         col.write(f"W-{iso_cal.week} D-{iso_cal.weekday}")
@@ -143,5 +150,6 @@ def strenght_training_sessions_view(sessions: List[model.WorkoutSession], desire
 
 
 excersises = st.text_input("Desired excercise [multiple with commas]", value="Squats")
+max_cols = st.number_input("max columns", value=12)
 desired_excercises = excersises.split(",")
-strenght_training_sessions_view(sessions_in_range, desired_excercises)
+strenght_training_sessions_view(sessions_in_range, desired_excercises, max_cols)
